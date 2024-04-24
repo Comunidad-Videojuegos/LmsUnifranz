aab5f7cb2e85314514503ddd7b0b38d7adf1c467
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-04-24 11:41:23 -0400

    Migration of reports

--- resources/views/components/nav-item.blade.php
+++ resources/views/components/nav-item.blade.php
@@ -1,13 +1,38 @@
 {{-- ITEM NAV --}}
 
-<a href="{{$link}}">
-    <div class="flex justify-around group-hover:bg-gray-800 py-1 rounded-e-3xl text-white">
-        <div class="flex justify-center items-center h-8 w-16 min-w-16
-        group-hover:scale-110 duration-75 ease-linear">
-            <x-bi-bell-fill class="text-primary text-center"/>
+<a href="{{route($link)}}">
+    <div class="flex justify-around hover:bg-gray-900 py-1 rounded-e-3xl
+        {{request()->is($link) ? 'bg-gray-900' : ''}}">
+        <div class="flex justify-center items-center h-8 w-20 min-w-20
+        group-hover:scale-110 duration-75 ease-linear
+        {{request()->is($link) ? 'scale-110' : ''}}">
+            @switch($icon)
+                @case("sliders")
+                    <x-bi-sliders/>
+                    @break
+                @case("calendar minus fill")
+                    <x-bi-calendar-minus-fill />
+                    @break
+
+                @case("people fill")
+                    <x-bi-people-fill />
+                @break
+
+                @case("wifi")
+                    <x-bi-wifi />
+                @break
+
+                @case("activity")
+                    <x-bi-activity />
+                @break
+
+                @default
+                    <x-bi-badge-sd-fill/>
+            @endswitch
         </div>
         <div class="flex justify-center items-center h-8 flex-grow
-        group-hover:text-lg duration-100 ease-linear hover:font-semibold">
+        group-hover:text-lg duration-100 ease-linear hover:font-semibold
+        {{request()->is($link) ? 'font-semibold text-lg' : ''}}">
             <h2 class="w-full">{{$title}}</h2>
         </div>
     </div>

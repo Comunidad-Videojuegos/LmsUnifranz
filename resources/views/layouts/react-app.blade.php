937e573d456c00def3d4302f7db5464c1fc123fc
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2024-05-13 22:20:02 -0400

    react fushion

--- resources/views/layouts/react-app.blade.php
+++ resources/views/layouts/react-app.blade.php
@@ -2,16 +2,14 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/png" href="{{asset('/assets/llantas-icon-7e8e1f56.png')}}" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Taller Mecanico</title>
-    <script type="module" crossorigin src="{{asset('/assets/index-33ad1911.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('/assets/index-2d48ce07.css')}}">
+    <title>PLATAFORMA</title>
+    <script type="module" crossorigin src="{{asset('/assets/index-CPxQjvCM.js')}}"></script>
+    <link rel="stylesheet" crossorigin href="{{asset('/assets/index-B_RrPX2x.css')}}">
   </head>
   <body>
     <div id="root"></div>
 
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   </body>
 </html>
